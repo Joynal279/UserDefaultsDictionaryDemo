27c91d6c774260397586c8a4d5b2316ddb19dd97
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-11-09 00:32:44 +0600

    initial commit

--- UserDefaultsDictionaryDemo/ViewController.swift
+++ UserDefaultsDictionaryDemo/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var dictionary:[String: Data] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // data append
+        dataAppendInDictionary()
+        
+        //Access Shared Defaults Object
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(dictionary, forKey: "myKey")
+        // Read/Get Dictionary
+        let strings:[String:Data] = userDefaults.object(forKey: "myKey") as? [String:Data] ?? [:]
+        
+        print(strings)
+        showImage(data: strings["1"]!)
     }
-
-
+    
+    func dataAppendInDictionary(){
+        dictionary.updateValue(UIImage(named: "imageD")!.pngData()!, forKey: "1")
+        dictionary.updateValue(UIImage(named: "imageD")!.pngData()!, forKey: "2")
+        dictionary.updateValue(UIImage(named: "imageD")!.pngData()!, forKey: "3")
+    }
+    func showImage(data: Data) {
+        let image = UIImageView(frame: CGRect(x: 10, y: 100, width: 100, height: 100))
+        self.view.addSubview(image)
+        image.image = UIImage(data: data)
+    }
 }
-
